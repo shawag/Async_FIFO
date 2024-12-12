62138d02907182cf570fa8e4e709fe19c9df9d3f
shawag <727627411@qq.com>
2024-12-12 21:35:20 +0800

    updata parameter for clear declaration

--- user/src/Gray_counter_style1.sv
+++ user/src/Gray_counter_style1.sv
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //generate n  and n+1 gray code
 module Gray_counter_style_1 #(
-    parameter   D_WIDTH = 8
+    parameter   PTR_WIDTH = 8
 ) 
 (
     input                   clk     ,
@@ -9,43 +9,43 @@ module Gray_counter_style_1 #(
     input                   inc     ,
     input                   full    ,
     input                   empty   ,
-    output [D_WIDTH:0]      ptr     ,
-    output [D_WIDTH-1:0]    addr 
+    output [PTR_WIDTH-1:0]      ptr     ,
+    output [PTR_WIDTH-2:0]    addr 
 );
 
-logic [D_WIDTH:0] bin;
-logic [D_WIDTH:0] bin_nxt;
-logic [D_WIDTH:0] g_nxt;
+logic [PTR_WIDTH-1:0] bin;
+logic [PTR_WIDTH-1:0] bin_nxt;
+logic [PTR_WIDTH-1:0] g_nxt;
 logic             msb_nxt;
 logic             addr_msb;
 //binary increment counter
 assign inc_num = inc & (~full)|(~empty);
 assign bin_nxt = bin + inc_num;
 //msb
-assign msb_nxt = ptr[D_WIDTH] ^ ptr[D_WIDTH-1];
-assign addr = {msb_nxt,ptr[D_WIDTH-2:0]};
+assign msb_nxt = ptr[PTR_WIDTH-1] ^ ptr[PTR_WIDTH-2];
+assign addr = {msb_nxt,ptr[PTR_WIDTH-3:0]};
 //gray to binary
-assign bin[D_WIDTH] = ptr[D_WIDTH];
+assign bin[PTR_WIDTH-1] = ptr[PTR_WIDTH-1];
 
 generate
     genvar i;
-    for(i=0; i<D_WIDTH; i=i+1) begin
+    for(i=0; i<PTR_WIDTH-1; i=i+1) begin
         assign bin[i] = ptr[i] ^ bin[i+1];
     end
 endgenerate
 
 //binary to gray
-assign g_nxt[D_WIDTH] = bin_nxt[D_WIDTH];
+assign g_nxt[PTR_WIDTH-1] = bin_nxt[PTR_WIDTH-1];
 generate
     genvar j;
-    for(j=0; j<D_WIDTH; j=j+1) begin
+    for(j=0; j<PTR_WIDTH-1; j=j+1) begin
         assign g_nxt[j] = bin_nxt[j+1] ^ bin_nxt[j];
     end        
 endgenerate
 
 always_ff @(posedge clk ,negedge rst_n) begin
     if(~rst_n)
-        ptr <= {D_WIDTH+1{1'b0}};
+        ptr <= {PTR_WIDTH{1'b0}};
     else
         ptr <= g_nxt;
 end
--- user/src/Gray_counter_style2.sv
+++ user/src/Gray_counter_style2.sv
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 module Gray_counter_style_2 #(
-    parameter    D_WIDTH = 8
+    parameter    PTR_WIDTH = 8
 )
 (
     input                   clk     ,
@@ -8,39 +8,39 @@ module Gray_counter_style_2 #(
     input                   inc     ,
     input                   full    ,
     input                   empty   ,
-    output [D_WIDTH-1:0]    addr     ,
-    output [D_WIDTH:0]      ptr     
+    output [PTR_WIDTH-2:0]  addr     ,
+    output [PTR_WIDTH-1:0]  ptr     
 );
 
-logic [D_WIDTH:0] bin;
-logic [D_WIDTH:0] bin_nxt;
+logic [PTR_WIDTH-1:0] bin;
+logic [PTR_WIDTH-1:0] bin_nxt;
 logic             inc_num;
-logic [D_WIDTH:0] g_nxt;
+logic [PTR_WIDTH-1:0] g_nxt;
 //addr logic
-assign addr = bin[D_WIDTH-1:0];
+assign addr = bin[PTR_WIDTH-2:0];
 //binarty increment counter
 assign inc_num = inc &((~full)|(~empty));
 assign bin_nxt = bin + inc_num;
 
 always_ff @(posedge clk, negedge rst_n) begin
     if(~rst_n)
-        bin <= {D_WIDTH+1{1'b0}};
+        bin <= {PTR_WIDTH{1'b0}};
     else
         bin <= bin_nxt;
 end
 
 //binary to gray
-assign g_nxt[D_WIDTH] = bin_nxt[D_WIDTH];
+assign g_nxt[PTR_WIDTH-1] = bin_nxt[PTR_WIDTH-1];
 generate
     genvar i;
-    for(i=0;i<D_WIDTH;i=i+1) begin
+    for(i=0;i<PTR_WIDTH-1;i=i+1) begin
         assign g_nxt[i] = bin_nxt[i+1] ^ bin_nxt[i];
     end
 endgenerate
 
 always_ff @(posedge clk, negedge rst_n) begin
     if(~rst_n)
-        ptr <= {D_WIDTH+1{1'b0}};
+        ptr <= {PTR_WIDTH{1'b0}};
     else
         ptr <= g_nxt;
 end
